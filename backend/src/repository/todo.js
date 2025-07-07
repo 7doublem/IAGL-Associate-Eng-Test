e4f44b32e10e48a2b94d0e1fd49a827f3f4a4536
7doublem <mustafamoallim@gmail.com>
2025-07-07 22:31:48 +0100

    update backend repository/service to support additional todo fields (description, start date, end date) and update backend tests for repository and service

--- backend/src/repository/todo.js
+++ backend/src/repository/todo.js
@@ -1,12 +1,44 @@
-
+// Updated todo task example to follow structure of future posted tasks
 let todoList = {
   todos: [
     {
-      "task": "This is a todo example"
-    }
-  ]
+      id: 1,
+      task: "This is a todo example",
+      description: "Example description",
+      startDate: "07/07/2025",
+      endDate: "08/07/2025",
+      createdAt: new Date().toLocaleDateString("en-GB"),
+      dateCompleted: null,
+    },
+  ],
 };
 
+// IDs starting for 2 since example id is 1
+
+let idCounter = 2;
+
 module.exports = {
-  getTodos: () => Promise.resolve(todoList)
+  // Get all todos
+  getTodos: () => Promise.resolve(todoList),
+
+  // Post new todo
+  postTodo: (todoData) => {
+    const newTodo = {
+      id: idCounter++,
+      task: todoData.task,
+      description: todoData.description,
+      // Format date to uk format if provided, otherwise null
+      startDate: todoData.startDate
+        ? new Date(todoData.startDate).toLocaleDateString("en-GB")
+        : null,
+      endDate: todoData.endDate
+        ? new Date(todoData.endDate).toLocaleDateString("en-GB")
+        : null,
+      createdAt: new Date().toLocaleDateString("en-GB"),
+      dateCompleted: null,
+    };
+    todoList.todos.push(newTodo);
+
+    return Promise.resolve(newTodo);
+  },
 };
--- backend/test/repository/todo.test.js
+++ backend/test/repository/todo.test.js
@@ -1,12 +1,19 @@
+const repository = require("../../src/repository/todo");
 
-const repository = require('../../src/repository/todo');
-
-describe('TODO repository', () => {
-  it('should return the todo list', async () => {
+describe("TODO repository", () => {
+  it("should return the todo list", async () => {
     const expected = {
-      todos: [{
-        "task": "This is a todo example"
-      }]
+      todos: [
+        {
+          id: 1,
+          task: "This is a todo example",
+          description: "Example description",
+          startDate: "07/07/2025",
+          endDate: "08/07/2025",
+          createdAt: new Date().toLocaleDateString("en-GB"),
+          dateCompleted: null,
+        },
+      ],
     };
     const actual = await repository.getTodos();
     expect(actual).toEqual(expected);
